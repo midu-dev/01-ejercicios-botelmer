4be3e053179630b129af436b09bdfa2367a980d0
Elmer EM. Mamani <elmer.mamani@REDSALUD.IQUIQUE.LOCAL>
2023-07-21 11:35:12 -0400

    .

--- index.js
+++ index.js
@@ -2,34 +2,36 @@ const fs = require('node:fs/promises');
 
 // Ejercicio 2
 async function writeFile (filePath, data, callback) {
-  
   const createDir = await fs.mkdir(filePath, { recursive: true });
-  const createFile = await fs.writeFile(filePath, data, { encoding: 'utf8' })
-    .then(callback(null))
-    .catch((err)=>{
-      callback(err)
-    });
-}
-// Ejercicio 3
-async function readFileAndCount (word, callback) {
-    const readFile = await fs.readFile(process.argv[2], 'utf-8')
-    .then((text)=>{
-      let count = 0;
-      count = text.split('node').length-1;
-      callback(null, count);
+  const createFile = await fs.writeFile(filePath + '/file.txt', data, { encoding: 'utf8' })
+    .then(()=>{
+      callback(null);
     })
     .catch((err)=>{
-      if(word === undefined) {
-        err.message = 'No se ha especificado la palabra a buscar';
-      }
-      if(process.argv[2] === undefined) {
-        err.message = 'No se ha especificado el path del archivo';
-      }
-      if(err.code === 'ENOENT'){
-        err = null;
-      }
-      callback(err, 0);
+      callback(err);
     });
+}
+
+// Ejercicio 3
+async function readFileAndCount (word, callback) {
+  let count = 0;
+  const readFile = await fs.readFile(process.argv[2], 'utf-8')
+  .then((text)=>{
+    count = text.split('node').length-1;
+    callback(null, count);
+  })
+  .catch((err)=>{
+    if(word === undefined) {
+      err.message = 'No se ha especificado la palabra a buscar';
+    }
+    if(process.argv[2] === undefined) {
+      err.message = 'No se ha especificado el path del archivo';
+    }
+    if(err.code === 'ENOENT'){
+      err = null;
+    }
+    callback(err, count);
+  });
 }
 
 module.exports = {
